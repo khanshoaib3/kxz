ba5ce039b2cb11084de33f6123912bd4febdc3ba
Mohammad Shoaib Khan <khanshoaibpers@outlook.com>
2024-12-10 20:25:34 +0530

    feat: Added api to get images

--- backend/routes/imgRoutes.js
+++ backend/routes/imgRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const upload = require("../middlewares/file_storage");
 const auth = require("../middlewares/auth");
@@ -8,21 +9,71 @@ const Category = require("../models/catModel");
 
 /* Note: Following routes are prefixed with `/image/` */
 
-router.get("/all", (req, res) => {
+router.get("/all", async (req, res) => {
   try {
-    console.log("thala");
+    let { category } = req.body;
+    let raw_images = await Image.find(
+      {},
+      { _id: 1, title: 1, url: 1, likes: 1, user: 1, categories: 1 }
+    );
+    if (category) {
+      const cat = await Category.find({ name: category });
+      if (!cat) {
+        return res
+          .status(400)
+          .json({ error: `Category with name, ${category}, not found @o@` });
+      }
+    }
 
-    res.send("<h1>Img routes</h1>");
+    let images = [];
+    for (let i = 0; i < raw_images.length; i++) {
+      const img_user = await User.findById(raw_images[i].user);
+      if (!img_user) {
+        console.log(
+          `[Error] User with id, ${raw_images[i].user}, not found :)`
+        );
+        continue;
+      }
+
+      let cat_names = [];
+      for (let j = 0; j < raw_images[i].categories.length; j++) {
+        const cat = await Category.findById(raw_images[i].categories[j]);
+        if (!cat) {
+          console.log(
+            `[Error] Category with id, ${raw_images[i].categories[j]}, not found :)`
+          );
+          continue;
+        }
+        cat_names.push(cat.name);
+      }
+
+      if (category && !cat_names.includes(category)) {
+        continue;
+      }
+
+      img = {
+        id: raw_images[i]._id.toHexString(),
+        title: raw_images[i].title,
+        url: raw_images[i].url,
+        likes: raw_images[i].likes,
+        user: img_user.username,
+        categories: cat_names,
+      };
+
+      images.push(img);
+    }
+
+    res.json({ images: images });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-// NOTE: The request's content-type will be "multipart/form-data", so we'll have to create the array manually. In this case, we split the input by comma. 
+// NOTE: The request's content-type will be "multipart/form-data", so we'll have to create the array manually. In this case, we split the input by comma.
 router.post("/add", auth, upload.single("file"), async (req, res) => {
   try {
     let { title, categories } = req.body;
-    categories = categories.split(",")
+    categories = categories.split(",");
     let file = req.file;
 
     if (!title || !categories || categories.length < 0) {
@@ -40,11 +91,9 @@ router.post("/add", auth, upload.single("file"), async (req, res) => {
     for (let i = 0; i < categories.length; i++) {
       const cat = await Category.findOne({ name: categories[i].trim() });
       if (!cat) {
-        return res
-          .status(400)
-          .json({
-            error: `Category with name, ${categories[i]}, not found =_=`,
-          });
+        return res.status(400).json({
+          error: `Category with name, ${categories[i]}, not found =_=`,
+        });
       }
 
       category_ids.push(cat._id);
@@ -55,11 +104,11 @@ router.post("/add", auth, upload.single("file"), async (req, res) => {
       url: file.path,
       user: user._id,
       categories: category_ids,
-      likes: 0
+      likes: 0,
     });
 
     await img.save();
-    res.json({text: "Image added successfully ^v^"})
+    res.json({ text: "Image added successfully ^v^" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
